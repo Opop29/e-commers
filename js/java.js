a01cc66e7d78e5f8fa77d26c96a3d531cf553978
joshua <joshuaquiidit@gmail.com>
2024-02-04 20:39:36 +0800

    add_styleofconstainer_and_js for image produc

---
+++ js/java.js
@@ -0,0 +1,40 @@
+const product = [
+   { id: 0,
+    image:'./image/smile.jpg',
+    title: 'smilekachu',
+    price: 200,
+   },
+   { id: 1,
+    image:'./image/naruchu.jpg',
+    title: 'naruchu',
+    price: 400,
+   },
+   { id: 2,
+    image:'./image/zinichu.jpg',
+    title: 'zinichu',
+    price: 450,
+   },
+   { id: 3,
+    image:'./image/ironchu.jpg',
+    title: 'ironchu',
+    price: 500,
+   }
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+     let i=0;
+     document.getElementById('root').innerHTML = categories.map((item)=>
+     {
+    var {image,title,price} = item;
+    return `
+    <div class='box'>
+        <div class='img-box'>
+            <img class='images' src='${image}'></img>
+        </div>
+        <div class='bottom'>
+            <p>${title}</p>
+            <h2>$ ${price}.00</h2>
+            <button onclick='addtocart(${i++})'>Add to cart</button>
+        </div>
+    </div>`;
+}).join('');
